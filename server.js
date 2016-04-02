f276010cefc9ecdd9aa3c71b082bec4c94f00758
Roderick Hsiao <roderickhsiao@gmail.com>
2016-04-01 21:31:17 -0700

    Add basic files

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hey, This is Roderick <roderickhsiao@gmail.com>');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('roderickhsiao.me app listening on port 3000!');
 });
