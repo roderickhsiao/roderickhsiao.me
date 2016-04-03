91985f2d47c7aaee3058ab55c0ca520ac083992b
Roderick Hsiao <roderickhsiao@gmail.com>
2016-04-03 01:26:15 -0700

    Add some material style

--- components/common/HeaderComponent.jsx
+++ components/common/HeaderComponent.jsx
@@ -6,23 +6,29 @@ import SocialPersonOutline from 'react-material-icons/icons/social/person-outlin
 class HeaderComponent extends Component {
     render () {
         return (
-            <div className='Bxz(bb) W(100%) Bgc($c-indigo-500) C(#fff) Py(30px) Px(20px) Bxsh($header-box-shadow)'>
-                <div className='Fz(56px) Lh(1.5) D(ib)'>Roderick Hsiao</div>
-                <div className='connect Fl(end) M(20px)'>
-                    <a href='mailto:roderickhsiao@gmail.com' title='email'>
-                        <EmailIcon
-                            className='Fill(#fff)!:h'
-                            color='rgba(255,255,255,.54)'
-                            style={{width:30, height:30}}
-                        />
-                    </a>
-                    <a download href='https://docs.google.com/uc?authuser=0&id=0B6if6pP4EgBiWTFaLV9PNVZnc0E&export=download' title='resume'>
-                        <SocialPersonOutline
-                            className='Fill(#fff)!:h Mstart(20px)'
-                            color='rgba(255,255,255,.54)'
-                            style={{width:30, height:30}}
-                        />
-                    </a>
+            <div className='Bxz(bb) W(100%) Bgc($c-indigo-500) C(#fff) Pb(30px) Px(20px) Bxsh($header-box-shadow)'>
+                <div className='Fz(56px) Lh(1.5) D(ib) Mt($top-header-height)'>
+                    Roderick Hsiao
+                </div>
+                <div className='top-header Pos(f) T(0) End(0) Start(0) Bgc($c-indigo-500) H($top-header-height)'>
+                    <div className='connect D(tb) Pos(a) End(0) H(100%)'>
+                        <a className='D(tbc) Va(m)' href='mailto:roderickhsiao@gmail.com' title='email'>
+                            <EmailIcon
+                                className='Fill(#fff)!:h Mx(20px)'
+                                color='rgba(255,255,255,.54)'
+                                style={{width:30, height:30}}
+                            />
+                        </a>
+                        <a
+                            className='D(tbc) Va(m)'
+                            download href='https://docs.google.com/uc?authuser=0&id=0B6if6pP4EgBiWTFaLV9PNVZnc0E&export=download' title='resume'>
+                            <SocialPersonOutline
+                                className='Fill(#fff)!:h Mx(20px)'
+                                color='rgba(255,255,255,.54)'
+                                style={{width:30, height:30}}
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
         );
--- configs/atomizer.js
+++ configs/atomizer.js
@@ -2,6 +2,7 @@ module.exports = {
     custom: {
         "$c-grey-50": "#fafafa",
         "$c-indigo-500": "#3f51b5",
-        "$header-box-shadow": "0 2px 5px rgba(0,0,0,0.26)"
+        "$header-box-shadow": "0 2px 5px rgba(0,0,0,0.26)",
+        "$top-header-height": "54px"
     }
 };
