8c3a486f2681880c9f460baddeae3783e8b6c14f
Roderick Hsiao <roderickhsiao@gmail.com>
2016-04-09 08:31:10 -0700

    Change to 8080 port

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ server.use(function (req, res, next) {
     });
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 server.listen(port);
 console.log('Listening on port ' + port);
