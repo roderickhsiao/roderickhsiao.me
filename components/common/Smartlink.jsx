05692063021d9637e83c958242a42cbe1df4bfb9
Roderick Hsiao <roderickhsiao@users.noreply.github.com>
2019-02-28 17:42:25 -0800

    Load simple module

--- components/common/Smartlink.jsx
+++ components/common/Smartlink.jsx
@@ -1,5 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
-import AspectRatio from 'react-aspect-ratio';
+import { AspectRatio } from 'react-aspect-ratio';
 
 import Img from './Img.jsx';
 
