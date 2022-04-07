59f0c291550a1a3795840396d2d7a8da21ce99cd
Roderick Hsiao <roderickhsiao@gmail.com>
2022-04-07 00:21:18 -0700

    Update style

--- components/common/Smartlink.jsx
+++ components/common/Smartlink.jsx
@@ -15,29 +15,37 @@ const Smartlink = memo((props) => {
   const isLargeTemplate = smartlink.type === 'large';
   return (
     <a
-      className="Bd My(10px) Bdc($c-black-4) Cf D(b) Cur(p) Td(n) Td(n):h Bxsh($shadow-2dp) Bxsh($shadow-4dp):h Trsdu($trsdu-fast)"
+      className={classNames(
+        'D(f) Jc(c) Bd My(10px) Bdc($c-black-4) Cf D(b) Cur(p) Td(n) Td(n):h Bxsh($shadow-2dp) Bxsh($shadow-4dp):h Trsdu($trsdu-fast)',
+        {
+          'Fxd(c)': isLargeTemplate,
+        }
+      )}
       href={smartlink.url}
       target="_black"
       rel="noopener noreferrer"
     >
       <Fragment>
         {isLargeTemplate ? (
-          <AspectRatio ratio={ratio}>
+          <AspectRatio ratio={ratio} style={{ width: '100%' }}>
             <Img src={thumbnail.url} alt="" width={670} />
           </AspectRatio>
         ) : (
-          <AspectRatio style={{ width: '150px' }}>
+          <AspectRatio
+            className="W(100px)--xs Fxs(0)"
+            style={{ width: '150px' }}
+          >
             <Img
               src={thumbnail.url}
               width={150}
               height={150}
-              className="M(-1px) H(100px)!--xs W(100px)!--xs D(ib) Fl(start) Bgc(#fff)"
+              className="D(ib) Bgc(#fff) Objf(ct)"
             />
           </AspectRatio>
         )}
         <div
-          className={classNames('P(10px) C($c-black-1) P(6px)!--xs', {
-            'Va(t) Mstart(160px) Mstart(100px)!--xs Bxz(bb)': !isLargeTemplate,
+          className={classNames('P(10px) C($c-black-1) P(6px)!--xs Fxg(1)', {
+            'Va(t)': !isLargeTemplate,
           })}
         >
           <div className="Fz(1.4em) Fz(1.2em)!--xs My(10px) My(2px)!--xs">
