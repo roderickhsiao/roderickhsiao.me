42e632a358d46607bcb43d3fdf116fbad2e0ba5d
Roderick Hsiao <roderickhsiao@gmail.com>
2022-04-07 01:47:27 -0700

    Move to partytown

--- components/Html.js
+++ components/Html.js
@@ -8,10 +8,13 @@ const inlineJSDetect = '(function(html){var c = html.className;c += " JsEnabled"
 const pathPrefix = '/';
 
 const inlineGA = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','UA-76603120-1');
 
+  gtag('js', new Date());
   gtag('config', 'UA-76603120-1');
 `;
 
@@ -48,9 +51,7 @@ export const headerStringStart = [
   '<link rel="apple-touch-icon" sizes="144x144" href="taz-2x.png" />',
   '<link rel="apple-touch-icon" sizes="192x192" href="taz-3x.png" />',
 
-  '<link rel="dns-prefetch" href="https://www.gstatic.com/" />',
   '<link rel="dns-prefetch" href="https://fonts.googleapis.com" />',
-  '<link rel="dns-prefetch" href="https://www.googletagmanager.com" />',
   '<link rel="dns-prefetch" href="https://cdn.polyfill.io" />',
   '<link rel="preconnect" href="https://fonts.googleapis.com" />',
   '<link rel="preconnect" href="https://cdn.polyfill.io" />',
