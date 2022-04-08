8ad200855e007eab9cf7dcfe0cde6fc150edb2a8
Roderick Hsiao <roderickhsiao@gmail.com>
2022-04-07 17:07:12 -0700

    Add scroll snap

--- components/common/Carousel.jsx
+++ components/common/Carousel.jsx
@@ -7,12 +7,13 @@ const Carousel = ({ nodes, aspectRatio = '16/9' }) => {
   }
 
   return (
-    <ul className="M(0) P(0) D(f) List(n) W(100%) Ovx(s)">
+    <ul className="M(0) P(0) D(f) List(n) W(100%) Ovx(s)" style={{ scrollSnapType: 'mandatory'; }}>
       {nodes.map((node, i) => {
         return (
           <li
             key={i}
             className="P(0) Mend(8px) Mend(0):lc"
+            style={{ scrollSnapAlign: 'start' }}
           >
             <AspectRatio className="W(240px)" ratio={aspectRatio}>
               {node}
