cf1ab978d64f7cfe1b0bd017b51ee592a84aed96
Roderick Hsiao <roderickhsiao@gmail.com>
2024-06-28 12:12:19 -0700

    fix: Remove polyfill.io

--- components/Html.js
+++ components/Html.js
@@ -17,7 +17,6 @@ const HtmlComponent = memo((props) => {
     <body className="Bgc($c-grey-50)">
       <div id="app" dangerouslySetInnerHTML={{ __html: markup }} />
       <script dangerouslySetInnerHTML={{ __html: state }} />
-      <script src="//cdn.polyfill.io/v3/polyfill.min.js?features=Intl.DisplayNames,Intl.DisplayNames.~locale.en,Intl.ListFormat,Intl.ListFormat.~locale.en" defer async />
       <script src={getHashAssets('/js/client.js')} defer async />
     </body>
   );
@@ -36,9 +35,7 @@ export const headerStringStart = [
   '<link rel="apple-touch-icon" sizes="192x192" href="taz-3x.png" />',
 
   '<link rel="dns-prefetch" href="https://fonts.googleapis.com" />',
-  '<link rel="dns-prefetch" href="https://cdn.polyfill.io" />',
   '<link rel="preconnect" href="https://fonts.googleapis.com" />',
-  '<link rel="preconnect" href="https://cdn.polyfill.io" />',
   '<link rel="preconnect" href="https://roderickhsiao.imgix.net" />',
   '<link rel="manifest" href="/manifest.json" />',
 
