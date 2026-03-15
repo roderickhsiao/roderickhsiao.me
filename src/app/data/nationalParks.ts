dddeb3868dbbf977904b98d1b35aadbcb99066d6
Roderick Hsiao <roderickhsiao@gmail.com>
2026-03-15 15:24:17 -0700

    fix typo

--- src/app/data/nationalParks.ts
+++ src/app/data/nationalParks.ts
@@ -15,8 +15,8 @@ export const NATIONAL_PARKS: Record<string, NationalPark[]> = {
     { name: 'deathValley', image: '/national-parks/death-valley.png' },
   ],
   TW: [
-    { name: 'taroko',       image: '/national-parks/taorko.png' },
-    { name: 'yangmingshan', image: '/national-parks/yangminshan.png' },
+    { name: 'taroko',       image: '/national-parks/taroko.png' },
+    { name: 'yangmingshan', image: '/national-parks/yangmingshan.png' },
     { name: 'kenting',      image: '/national-parks/kenting.png' },
   ],
 };
