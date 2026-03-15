8b29861446fe0fb216033e56927c2cb7b1794b2d
Roderick Hsiao <roderickhsiao@gmail.com>
2026-03-15 08:07:45 -0700

    correct city stamp

--- src/app/components/Travel/CityStamp.tsx
+++ src/app/components/Travel/CityStamp.tsx
@@ -88,7 +88,6 @@ export default function CityStamp({ city, countryCode, stampColor, index }: City
             height="20"
             viewBox="0 0 24 24"
             className="fill-current"
-            style={{ transform: 'rotate(180deg)' }}
           >
             <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
           </svg>
